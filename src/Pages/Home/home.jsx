75fb0d00a37d59d33ecab06b656effcc5690ef3d
ShengYuan-BeSqaure <sheng.yuan@besquare.com.my>
2022-11-09 07:35:04 +0000

    made amendment on signup and login page

--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { useLocation, Link } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
-import "../home/home.css";
+import "./home.css";
 
 function Home() {
+  const navigate = useNavigate();
+
   return (
     <div>
       <div className="home-header">
@@ -15,18 +17,21 @@ function Home() {
             Stay home, we always deliver a service <br></br> more than you
             expected.
           </p>
-          <button>SHOP NOW</button>
+          <button onClick={() => navigate("/marketplace")}>Shop Now</button>
         </div>
 
         <div className="home-image-container">
-          <img src="https://images.unsplash.com/photo-1584473457406-6240486418e9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80" />
+          <img
+            src="https://images.unsplash.com/photo-1584473457406-6240486418e9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80"
+            alt="grocery-bag-img"
+          />
         </div>
       </div>
       <div className="today-top-deals">
         <div className="today-top-deals-title">
           <h4>Today's Top Deals</h4>
           <div className="view-more-button">
-            <p>View More</p>
+            <p onClick={() => navigate("/deals-&-promotions")}>View More</p>
             <ArrowRightAltIcon></ArrowRightAltIcon>
           </div>
         </div>
@@ -40,7 +45,10 @@ function Home() {
 
       <div className="promotion-banner">
         <div className="banner-image-container">
-          <img src="https://images.unsplash.com/photo-1487646709898-58d3c6e8d886?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" />
+          <img
+            src="https://images.unsplash.com/photo-1487646709898-58d3c6e8d886?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+            alt="promotion-banner-img"
+          />
         </div>
 
         <div className="promotion-banner-content">
@@ -64,7 +72,7 @@ function Home() {
         <div className="best-choices-title">
           <h4>Best Choices</h4>
           <div className="view-more-button">
-            <p>View More</p>
+            <p onClick={() => navigate("/best-sellers")}>View More</p>
             <ArrowRightAltIcon></ArrowRightAltIcon>
           </div>
         </div>
@@ -75,13 +83,19 @@ function Home() {
             <p>hello world</p>
           </div>
           <div className="category-card">
-            <p>hello world</p>
+            <img
+              src="https://images.unsplash.com/photo-1582401656496-9d75f95f9018?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80"
+              alt="cereal-img"
+            />
           </div>
         </div>
 
         <div className="best-choices-product-2">
           <div className="category-card">
-            <p>hello world</p>
+            <img
+              src="https://images.unsplash.com/photo-1607623814075-e51df1bdc82f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+              alt="raw-meat-img"
+            />
           </div>
           <div className="large-horizontal-card">
             <p>hello world</p>
@@ -89,13 +103,38 @@ function Home() {
         </div>
       </div>
 
-      <div className="services-content">
+      <div className="separator">
         <hr></hr>
+      </div>
+
+      <div className="services-content">
         <div className="services-content-container">
-          <div className="services-content-container-title">
+          <div className="services-content-title">
             <h4>Start Shopping With Us</h4>
           </div>
-          <div className="services-content-container-text"></div>
+
+          {/* <div className="services">
+            <div className="services-1">
+              <p className="services-title">Monthly Exciting Sales</p>
+              <p className="services-text">
+                Limited time promotio every month!
+              </p>
+            </div>
+            <div className="services-2">
+              <p className="services-title">Monthly Exciting Sales</p>
+              <p className="services-text">
+                Limited time promotio every month!
+              </p>
+            </div>
+            <div className="services-3">
+              <p className="services-title">Monthly Exciting Sales</p>
+              <p className="services-text">
+                Limited time promotio every month!
+              </p>
+            </div>
+          </div> */}
+
+          <button>Shop Now</button>
         </div>
       </div>
     </div>
