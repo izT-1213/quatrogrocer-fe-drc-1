15fa6c886661e39bcac38e4967f093cac49b5a36
cornsnakeftw <marianne@besquare.com.my>
2022-11-17 11:28:38 +0800

    responsive footer

--- src/Pages/ProductPage/product-page.jsx
+++ src/Pages/ProductPage/product-page.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-
+import { useParams } from "react-router-dom";
 import { Carousel } from "react-responsive-carousel";
 import {
   AddShoppingCart,
