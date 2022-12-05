5c5a0bcd3e19ead23f452a30a5e153d0d67ae1df
Nurul Izzati <izzati@besquare.com.my>
2022-12-05 05:01:43 +0000

    api

--- src/function.jsx
+++ src/function.jsx
@@ -3,7 +3,7 @@ import Axios from "axios";
 async function LoginFunc(email, pass) {
   try {
     const response = await Axios.post(
-      "http://localhost:5000/quatro_user/login",
+      "https://api.quatrogrocer.one/quatro_user/login",
       {
         email: email,
         password: pass,
@@ -20,7 +20,7 @@ async function LoginFunc(email, pass) {
 async function RegisterFunc(email, pass, fname, lname, dob, gender) {
   try {
     const response = await Axios.post(
-      "http://localhost:5000/quatro_user/create",
+      "https://api.quatrogrocer.one/quatro_user/create",
       {
         email: email,
         password: pass,
@@ -38,7 +38,7 @@ async function RegisterFunc(email, pass, fname, lname, dob, gender) {
 async function FetchUser(userId) {
   try {
     const response = await Axios.post(
-      `http://localhost:5000/quatro_user/search?user_id=${userId}`
+      `https://api.quatrogrocer.one/quatro_user/search?user_id=${userId}`
     );
     return response.data.result;
   } catch (err) {
@@ -50,7 +50,7 @@ async function FetchUser(userId) {
 async function SearchProduct(product) {
   try {
     const response = await Axios.get(
-      "http://localhost:5000/quatro_product/get",
+      "https://api.quatrogrocer.one/quatro_product/get",
       {
         params: { product: product.toString() },
       }
@@ -67,7 +67,7 @@ async function GetUserAddress(user_id, address_id) {
   debugger;
   try {
     const response = await Axios.get(
-      "http://localhost:5000/quatro_address/get",
+      "https://api.quatrogrocer.one/quatro_address/get",
       {
         params: { user_id: user_id, address_id: address_id },
       }
@@ -91,7 +91,7 @@ async function CreateAddressFunc(
   try {
     console.log(addLine1);
     const response = await Axios.post(
-      "http://localhost:5000/quatro_address/create",
+      "https://api.quatrogrocer.one/quatro_address/create",
 
       // { withCredentials: true }, //hassif port 3002
 
@@ -121,7 +121,7 @@ async function UpdateAddressFunc(
 ) {
   try {
     const response = await Axios.post(
-      "http://localhost:5000/quatro_address/update_details",
+      "https://api.quatrogrocer.one/quatro_address/update_details",
       //hassif port 3002
       {
         address_line_1: addLine1,
@@ -143,7 +143,7 @@ async function DeleteAddressFunc(address_id) {
   try {
     console.log(address_id);
     const response = await Axios.delete(
-      `http://localhost:5000/quatro_address/delete?address_id=${address_id}`
+      `https://api.quatrogrocer.one/quatro_address/delete?address_id=${address_id}`
     );
     return response.status;
   } catch (err) {
@@ -154,7 +154,7 @@ async function DeleteAddressFunc(address_id) {
 async function FetchProduct() {
   try {
     const response = await Axios.get(
-      "http://localhost:5000/quatro_product/get"
+      "https://api.quatrogrocer.one/quatro_product/get"
       //{ withCredentials: true }
     );
     return response.data.result;
@@ -166,7 +166,7 @@ async function FetchProduct() {
 async function FetchDiscountProduct() {
   try {
     const response = await Axios.get(
-      "http://localhost:5000/quatro_product_discount/get"
+      "https://api.quatrogrocer.one/quatro_product_discount/get"
       //{ withCredentials: true }
     );
     return response.data.result;
@@ -185,7 +185,7 @@ async function UpdateProfileFunc(
 ) {
   try {
     await Axios.post(
-      "http://localhost:5000/quatro_user/update",
+      "https://api.quatrogrocer.one/quatro_user/update",
 
       {
         first_name: first_name,
@@ -206,7 +206,7 @@ async function UpdatePasswordFunc(oldPassword, password, user_id) {
   console.log(password ? true : false);
   try {
     await Axios.post(
-      "http://localhost:5000/quatro_user/update_password",
+      "https://api.quatrogrocer.one/quatro_user/update_password",
 
       {
         oldPassword: oldPassword,
@@ -223,7 +223,7 @@ async function UpdatePasswordFunc(oldPassword, password, user_id) {
 async function AddToCartFunc(user_id, product_id, product_quantity) {
   try {
     const response = await Axios.post(
-      "http://localhost:5000/quatro_cart/create",
+      "https://api.quatrogrocer.one/quatro_cart/create",
       {
         user_id: user_id,
         product_id: product_id,
@@ -245,7 +245,7 @@ async function AddToCartDiscFunc(
 ) {
   try {
     await Axios.post(
-      "http://localhost:5000/quatro_cart/create_discount",
+      "https://api.quatrogrocer.one/quatro_cart/create_discount",
 
       {
         user_id: user_id,
@@ -261,7 +261,7 @@ async function AddToCartDiscFunc(
 async function FetchTransaction(user_id) {
   try {
     const response = await Axios.get(
-      "http://localhost:5000/quatro_transaction/get_details/" + user_id
+      "https://api.quatrogrocer.one/quatro_transaction/get_details/" + user_id
       //{ withCredentials: true }
     );
     return response.data.result;
@@ -274,7 +274,7 @@ async function CheckoutProcess(user_id) {
   try {
     const response1 = await Axios.post(
       // "https://api.quatrogrocer.one/quatro_cart/push"
-      "http://localhost:5000/quatro_cart/push",
+      "https://api.quatrogrocer.one/quatro_cart/push",
       { user_id: user_id }
     );
 
@@ -282,7 +282,7 @@ async function CheckoutProcess(user_id) {
       try {
         const response2 = await Axios.post(
           // "https://api.quatrogrocer.one/quatro_transaction/update"
-          "http://localhost:5000/quatro_transaction/update",
+          "https://api.quatrogrocer.one/quatro_transaction/update",
           { user_id: user_id }
         );
 
@@ -290,7 +290,7 @@ async function CheckoutProcess(user_id) {
           try {
             const response = await Axios.post(
               // "https://api.quatrogrocer.one/quatro_transaction/checkout"
-              "http://localhost:5000/quatro_transaction/checkout",
+              "https://api.quatrogrocer.one/quatro_transaction/checkout",
               { user_id: user_id }
             );
             console.log(response.data.result);
@@ -312,7 +312,7 @@ async function DeleteCart(user_id) {
   try {
     await Axios.post(
       // "https://api.quatrogrocer.one/quatro_cart/delete"
-      "http://localhost:5000/quatro_cart/delete",
+      "https://api.quatrogrocer.one/quatro_cart/delete",
       { user_id: user_id }
     );
   } catch (err) {}
@@ -322,7 +322,7 @@ async function PaidCheckout(user_id) {
   try {
     const response = await Axios.post(
       // "https://api.quatrogrocer.one/quatro_transaction/update_payment"
-      "http://localhost:5000/quatro_transaction/update_payment",
+      "https://api.quatrogrocer.one/quatro_transaction/update_payment",
       { user_id: user_id }
     );
     return response;
