c4e2ca961acb5a5437cb91f1f11e1e83407b5db9
Nurul Izzati <izzati@besquare.com.my>
2022-12-05 02:46:13 +0000

    dob

--- src/Pages/SignUp/signup.jsx
+++ src/Pages/SignUp/signup.jsx
@@ -246,7 +246,7 @@ function SignUpPage() {
                     values.firstName &&
                     values.lastName &&
                     values.gender &&
-                    values.dob &&
+                    dob &&
                     values.checked
                       ? false
                       : true
